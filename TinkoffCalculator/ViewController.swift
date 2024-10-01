e727a775accb319524ae0f8eebdb63006c62d4db
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-01 14:05:05 +0400

    Initial Commit

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Hakob Ghlijyan on 01.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TinkoffCalculatorTests/TinkoffCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinkoffCalculatorTests.swift
+//  TinkoffCalculatorTests
+//
+//  Created by Hakob Ghlijyan on 01.10.2024.
+//
+
+import Testing
+@testable import TinkoffCalculator
+
+struct TinkoffCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
